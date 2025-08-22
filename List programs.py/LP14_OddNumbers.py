ecfb77b87f3144fc5380c72ffc0c622b037e7348
abhivaadyasharma <abhivaadya.sharma@gmail.com>
2025-08-22 17:08:58 +0530

    Program to find odd numbers in a list

---
+++ List programs.py/LP14_OddNumbers.py
@@ -0,0 +1,13 @@
+#Autor :Abhivaadya Sharma 
+
+# Get input from the user (comma-separated numbers)
+user_input = input("Enter numbers separated by spaces: ")
+
+# Convert the input string to a list of integers
+numbers = list(map(int, user_input.split()))
+
+# Print only the odd numbers
+print("Odd numbers in the list are:")
+for num in numbers:
+    if num / 2 != 0:
+        print(num)
